84c6226fed9d9fec6d42f01178456793c4e7f44b
antonio <tgq.spm@gmail.com>
2015-02-15 02:17:36 +0100

    proportional method added

--- functions.py
+++ functions.py
@@ -26,3 +26,28 @@ def hondt_method(votes, n_seats, cutoff=0):
     seats = seats.astype(int)
 
     return seats
+
+
+def proportional_method(votes, n_seats, cutoff=0):
+    """
+    """
+
+    n_total_votes = votes.sum()
+    abs_cutoff = cutoff*n_total_votes
+
+    computable_votes = np.zeros(votes.shape)
+    computable_votes[votes >= abs_cutoff] = votes[votes >= abs_cutoff]
+
+    n_comp_votes = computable_votes.sum()
+    ratio = computable_votes/n_comp_votes
+    seats = np.floor(ratio)
+    residuals = ratio-seats
+
+    for i in range(n_seats-seats.sum()):
+        s = np.argmax(residuals)
+        residuals[s] = 0
+        seats[s] += 1
+
+    return seats
+    
+    
