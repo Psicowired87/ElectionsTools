05d60cf39727b70006f266bb07fba8bff1d7cc50
antonio <tgq.spm@gmail.com>
2015-12-16 09:40:42 +0100

    Installation files.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Install
+sudo python setup.py install
+# Deleting trash files
+sudo rm -r build/
+sudo rm -r dist/
+sudo rm -r ElectionsTools.egg-info/
+
---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env/python
+
+"""
+Installation script
+"""
+
+
+import sys
+import os
+import warnings
+
+## Definition of useful functions
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def write_version_py(filename=None):
+    cnt = """\
+version = '%s'
+"""
+    if not filename:
+        filename = os.path.join(
+            os.path.dirname(__file__), 'ElectionsTools', 'version.py')
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % (VERSION))
+    finally:
+        a.close()
+
+
+## Check problems with the setuptools
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+## Quantify the version
+MAJOR = 0
+MINOR = 0
+MICRO = 0
+ISRELEASED = False
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+QUALIFIER = ''
+
+write_version_py()
+
+## Setup
+setup(name='ElectionsTools',
+      version=VERSION,
+      description='Utils for elections data analysis.',
+      license='BSD',
+      author='T. Gonzalez Quintela',
+      author_email='tgq.spm@gmail.com',
+      url='',
+      long_description=read('README.md'),
+      packages=[],
+      install_requires=['numpy', 'scipy', 'matplotlib', 'pandas']#,
+			#'datetime', #'python-mpltoolkits.basemap'
+			#'python-dateutil'],
+)
+
